dca2f55ec1dc59ad9cc6e9170a9b67de7d27a32c
jtzero ? <jtzero511@gmail.com>
2020-06-22 00:03:18 -0600

    init radio control

---
+++ src/RadioControlKnob/RadioControlKnob.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RadioControlKnob.css';
+
+const RadioControlKnob = () => {
+    return (
+        <div className="knob-body radial">
+            <div className="handle" draggable="true"></div>
+        </div>
+    )
+};
+
+export default RadioControlKnob;
